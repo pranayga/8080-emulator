94e1641a2d3c4e6262009fee67c894f0c44fe343
Pranay Garg <pranayga@andrew.cmu.edu>
2020-12-21 00:55:58 -0500

    NEW FILE: Space.c, main driver of invaders game
    This program defines the main function to run the invaders
    game

---
+++ 8080_emu/src/space.c
@@ -0,0 +1,134 @@
+/**
+ * @file space.c
+ * @author Pranay Garg (pranayga@andrew.cmu.edu)
+ * @brief The main driver to run invaders, the ATARI game.
+ * @version 0.1
+ * @date 2020-12-20
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include "cpu_8080.h"
+#include "debug.h"
+
+#define NUM_ROM_FILES 4
+#define ROM_CHUNK_SIZE 0x800
+
+// Major Helper Functions
+int mmap_invaders(char *path, intptr_t base);
+// Helper of Helpers Functions
+int check_open_roms(char *path, int* FDs);
+
+
+int main(){
+    debug_print("PRKS 8080 Emulator to run Space Invaders....");
+    WARN(0, "Hello from Warning....")
+
+    // initializing a New CPU instance
+    cpu_state* cpu = init_cpu_8080(0);      // For now don't know where PC initially points
+    cpu->base = aligned_alloc(16, 1<<15);   // Alloc aligned 64 KB
+
+    // memory Map the ROM
+    char* rom_path = "./invaders_rom";
+    int* rom_FDs;
+    if ((rom_FDs = load_invaders(rom_path, cpu->base)) == NULL){
+        fprintf(stderr, "Critical Error: Rom Load Failed.\n");
+        exit(-1);
+    }
+
+    // TODO: Do the exec here.
+
+    // Unmap the files and free the buffers.
+    for(uint8_t itr=0; itr<NUM_ROM_FILES; itr++){ // Map h, g, f ,e <0-3>
+        unmapstatus(cpu->base+(ROM_CHUNK_SIZE*itr), ROM_CHUNK_SIZE); // UnMap invaders.x
+        close(rom_FDs[itr]);
+    }
+    free(cpu->base);
+    free(cpu);
+    return 0;
+}
+
+// Helper Functions
+
+/**
+ * @brief Memory maps the invaders ROM into the correct memory locations.
+ * The way memory is mapped is documented at: http://www.emutalk.net/threads/38177-Space-Invaders
+ * 
+ * @param path to the folder containing the 4 ROM Chunks
+ * @param base pointer to the CPU instance executing the ROM
+ * @return int* NULL if fail, array of file descriptors if pass
+ */
+int* load_invaders(char *path, intptr_t base){
+    ASSERT(base!=NULL);
+    ASSERT(path!=NULL)
+    DEBUG_PRINT("Invaders Path: %s, BasePTR: %p", path, base);
+
+    // Check & Open All Roms
+    int* FDs = (int *)calloc(NUM_ROM_FILES, sizeof(int));
+    if(!check_open_roms(path, FDs)){
+        WARN(0, "ROM open failed.\n");
+        return 0;
+    }
+
+    // Memory mapping
+    for(uint8_t itr=0; itr<NUM_ROM_FILES; itr++){ // Map h, g, f ,e <0-3>
+        mmap(base+(ROM_CHUNK_SIZE*itr), ROM_CHUNK_SIZE, PROT_READ, MAP_PRIVATE, FDs[itr], 0); // Map invaders.x
+    }
+
+    return FDs;
+}
+
+// Helper of Helper Functions
+
+int check_open_roms(char* path, int* FDs){
+    struct stat mmapstat;
+    char file_path[256];
+    
+    sprintf(file_path, "%s/%s", path, "invaders.h");
+    if (stat(file_path, &mmapstat) == -1) {
+        WARN("stat failure");
+        return 0;
+    }
+    if ((FDs[0] = open(file_path, O_RDONLY)) == -1) {
+        WARN("open failure");
+        return 0;
+   }
+
+   sprintf(file_path, "%s/%s", path, "invaders.g");
+    if (stat(file_path, &mmapstat) == -1) {
+        WARN("stat failure");
+        return 0;
+    }
+    if ((FDs[1] = open(file_path, O_RDONLY)) == -1) {
+        WARN("open failure");
+        return 0;
+   }
+
+   sprintf(file_path, "%s/%s", path, "invaders.f");
+    if (stat(file_path, &mmapstat) == -1) {
+        WARN("stat failure");
+        return 0;
+    }
+    if ((FDs[2] = open(file_path, O_RDONLY)) == -1) {
+        WARN("open failure");
+        return 0;
+   }
+
+   sprintf(file_path, "%s/%s", path, "invaders.e");
+    if (stat(file_path, &mmapstat) == -1) {
+        WARN("stat failure");
+        return 0;
+    }
+    if ((FDs[3] = open(file_path, O_RDONLY)) == -1) {
+        WARN("open failure");
+        return 0;
+   }
+}
